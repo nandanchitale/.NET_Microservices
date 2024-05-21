9df199ff7ad0a094208f90ada99dd970b14973ab
Nandan <nandan.chitale@datastack.tech>
2024-05-21 19:05:51 +0530

    Added kubernetes file for commands project

--- PlatformService/obj/Debug/net8.0/PlatformService.AssemblyInfo.cs
+++ PlatformService/obj/Debug/net8.0/PlatformService.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PlatformService")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+efdd71371fb503589d38166edfe1e26e4ebbf2e8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c5f08c1d41d10a2003d6aa3b95bbb75f419e3205")]
 [assembly: System.Reflection.AssemblyProductAttribute("PlatformService")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PlatformService")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
