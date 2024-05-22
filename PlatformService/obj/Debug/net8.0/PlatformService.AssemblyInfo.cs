19c00801d58629c05b1a9e69affec7fef9acaf2b
Nandan <nandan.chitale@datastack.tech>
2024-05-22 15:33:05 +0530

    Added ingress nginx config file

--- PlatformService/obj/Debug/net8.0/PlatformService.AssemblyInfo.cs
+++ PlatformService/obj/Debug/net8.0/PlatformService.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PlatformService")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9df199ff7ad0a094208f90ada99dd970b14973ab")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6e54af96296c6ec263071f6907e6755d2abb22e8")]
 [assembly: System.Reflection.AssemblyProductAttribute("PlatformService")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PlatformService")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
