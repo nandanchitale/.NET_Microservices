1b2725ae5ccd12ed57bdbe9d418c79ce1b047013
Nandan <nandan.chitale@datastack.tech>
2024-05-24 16:01:53 +0530

    "Added GetCommandForPlatform and CreateCommandForPlatform endpoints to CommandsController,"

--- CommandsService/Controller/CommandsController.cs
+++ CommandsService/Controller/CommandsController.cs
@@ -41,4 +41,64 @@ public class CommandsController : ControllerBase
         }
         return response;
     }
+
+    [HttpGet("{commandId}", Name = "GetCommandForPlatform")]
+    public IActionResult GetCommandForPlatform(int platformId, int commandId)
+    {
+        IActionResult response = NotFound();
+        try
+        {
+            Console.WriteLine($"--> Hit GetCommandsForPlatform : {platformId} / {commandId}");
+            if (_repository.PlatFormExists(platformId))
+            {
+                Command command = _repository.GetCommand(platformId, commandId);
+                if (command is not null)
+                {
+                    response = Ok(
+                        _mapper.Map<CommandReadDto>(command)
+                    );
+                }
+            }
+        }
+        catch (Exception ex)
+        {
+            Console.BackgroundColor = ConsoleColor.Red;
+            Console.WriteLine($"Exception at Commands > GetCommandForPlatform() => {ex.Message}");
+            Console.BackgroundColor = ConsoleColor.Black;
+        }
+        return response;
+    }
+
+    [HttpPost]
+    public IActionResult CreateCommandForPlatform(int platformId, CommandCreateDto commandDto)
+    {
+        IActionResult response = NotFound();
+        try
+        {
+            Console.WriteLine($"--> Hit CreateCommandForPlatform : {platformId}");
+            if (_repository.PlatFormExists(platformId))
+            {
+                Command command = _mapper.Map<Command>(commandDto);
+                _repository.CreateCommand(platformId, command);
+                _repository.saveChanges();
+
+                CommandReadDto commandReadDto = _mapper.Map<CommandReadDto>(command);
+                response = CreatedAtRoute(
+                    nameof(GetCommandForPlatform),
+                    new {
+                        platformId = platformId,
+                        commandId = commandReadDto.Id
+                    }, 
+                    commandReadDto
+                );
+            }
+        }
+        catch (Exception ex)
+        {
+            Console.BackgroundColor = ConsoleColor.Red;
+            Console.WriteLine($"Exception at Commands > CreateCommandForPlatform() => {ex.Message}");
+            Console.BackgroundColor = ConsoleColor.Black;
+        }
+        return response;
+    }
 }
--- PlatformService/obj/Debug/net8.0/PlatformService.AssemblyInfo.cs
+++ PlatformService/obj/Debug/net8.0/PlatformService.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PlatformService")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+19c00801d58629c05b1a9e69affec7fef9acaf2b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fadf15abcfe1546ce2d438a2ad813ab32c0febb9")]
 [assembly: System.Reflection.AssemblyProductAttribute("PlatformService")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PlatformService")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
