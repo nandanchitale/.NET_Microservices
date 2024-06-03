8f8859e50785f15005dc293090ff79e03a5f13f5
Nandan <nandan.chitale@datastack.tech>
2024-06-03 18:42:38 +0530

    Minor changes in cheatsheet

--- PlatformService/obj/Debug/net8.0/PlatformService.AssemblyInfo.cs
+++ PlatformService/obj/Debug/net8.0/PlatformService.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PlatformService")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6be27538ab8a86102cda30a47565634afb0e9d5e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c023070eb8191bbc3b9f3bd35e297314e4f350b5")]
 [assembly: System.Reflection.AssemblyProductAttribute("PlatformService")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PlatformService")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
